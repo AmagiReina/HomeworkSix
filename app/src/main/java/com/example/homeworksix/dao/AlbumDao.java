a21b75a360285cc550bd6618f054c2f573849008
AmagiReina <nupbayev93@gmail.com>
2019-12-24 13:57:12 +0600

    Add Dao for entity Album

---
+++ app/src/main/java/com/example/homeworksix/dao/AlbumDao.java
@@ -0,0 +1,47 @@
+package com.example.homeworksix.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.TypeConverters;
+import android.arch.persistence.room.Update;
+import com.example.homeworksix.converters.DateConvert;
+import com.example.homeworksix.entity.Album;
+import java.sql.Date;
+import java.util.List;
+
+@Dao
+public interface AlbumDao {
+
+    // Выборка по id фильма
+    @Query("SELECT * FROM album WHERE id = :id")
+    Album getAlbumById(Long id);
+
+    // Выборка по названию альбома (возвращает список)
+    @Query("SELECT * FROM album WHERE album.title = :title")
+    List<Album> getAlbumByTitle(String title);
+
+    // Выборка по исполнителю (возвращает список)
+    @Query("SELECT * FROM album WHERE album.leadSinger = :singer")
+    List<Album> getAlbumBySinger(String singer);
+
+    // Выборка по дате выхода альбома
+    // (возвращает список, для даты используется конверетер)
+    @Query("SELECT * FROM album WHERE album.date = :date")
+    @TypeConverters(DateConvert.class)
+    List<Album> getAlbumByDate(Date date);
+
+    // Вставка фильма в базу данных
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void save(Album album);
+
+    // Обновление данных о фильме
+    @Update
+    void update(Album album);
+
+    // Удаление фильма
+    @Delete
+    void delete(Album album);
+}
